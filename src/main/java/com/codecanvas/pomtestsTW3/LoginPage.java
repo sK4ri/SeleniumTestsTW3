c6514a5db72ddd2b4b5ac5b02f3b553a4f912914
patrickbakos <bakospatrick@gmail.com>
2019-09-02 13:58:22 +0200

    Login testcases (parameterized login tests, login with missing credentials, login with enter)

---
+++ src/main/java/com/codecanvas/pomtestsTW3/LoginPage.java
@@ -0,0 +1,68 @@
+package com.codecanvas.pomtestsTW3;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import waiter.Waiter;
+
+public class LoginPage {
+
+    private Waiter waiter = new Waiter();
+    private final String PAGE_URL = "https://jira.codecool.codecanvas.hu/login.jsp";
+
+    @CacheLookup
+    @FindBy(id = "login-form-username") private WebElement usernameInputField;
+    @CacheLookup
+    @FindBy(id = "login-form-password") private WebElement passwordInputField;
+    @CacheLookup
+    @FindBy(id = "login") private WebElement loginButton;
+    @FindBy(id = "header-details-user-fullname") private WebElement userLogo;
+    @FindBy(xpath = "//p[contains(.,'Sorry')]") private WebElement loginErrorMessage;
+
+    public LoginPage (WebDriver driver) {
+
+        PageFactory.initElements(driver, this);
+        driver.get(PAGE_URL);
+    }
+
+    private boolean loginSuccess (WebDriver driver, WebElement webElement) {
+
+        try {
+            waiter.waitForElementToBeDisplayed(webElement, driver, 10);
+            return true;
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    public boolean login (WebDriver driver, String username, String password, String success) {
+
+        usernameInputField.sendKeys(username);
+        passwordInputField.sendKeys(password);
+        loginButton.click();
+
+        if ("true".equals(success)) {
+            return loginSuccess(driver, userLogo);
+        }
+        else if ("false".equals(success)) {
+            return loginSuccess(driver, loginErrorMessage);
+        } else return false;
+    }
+
+    public boolean loginWithMissingCredentials (WebDriver driver) {
+
+        loginButton.click();
+        return loginSuccess(driver, loginErrorMessage);
+    }
+
+    public boolean loginWithEnter (WebDriver driver, String username, String password) {
+
+        usernameInputField.sendKeys(username);
+        passwordInputField.sendKeys(password + Keys.RETURN);
+        return loginSuccess(driver, userLogo);
+    }
+}
