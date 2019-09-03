d26bf97b538e5de554b78e7ef31ca105f6bc3551
sK4ri <skari.simon@gmail.com>
2019-09-03 10:51:53 +0200

    Not working yet

--- src/main/java/com/codecanvas/pomtestsTW3/DashboardPage.java
+++ src/main/java/com/codecanvas/pomtestsTW3/DashboardPage.java
@@ -1,5 +1,10 @@
 package com.codecanvas.pomtestsTW3;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import waiter.Waiter;
 
 public class DashboardPage {
@@ -7,5 +12,22 @@ public class DashboardPage {
     private Waiter waiter = new Waiter();
     private final String PAGE_URL = "https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa";
 
+    @CacheLookup
+    @FindBy(id = "header-details-user-fullname") private WebElement userMenu;
+    @CacheLookup
+    @FindBy(id = "log-out") private WebElement logoutButton;
 
+    public DashboardPage(WebDriver driver) {
+
+        PageFactory.initElements(driver, this);
+        driver.get(PAGE_URL);
+    }
+
+    public void logout(WebDriver driver) {
+
+        userMenu.click();
+        waiter.waitForElementToBeDisplayed(logoutButton, driver, 10);
+        logoutButton.click();
+        waiter.waitForPageLoadComplete(driver, 10);
+    }
 }
--- src/test/java/LogoutTestCases.java
+++ src/test/java/LogoutTestCases.java
@@ -1,2 +1,39 @@
+import com.codecanvas.pomtestsTW3.DashboardPage;
+import com.codecanvas.pomtestsTW3.LoginPage;
+import com.codecanvas.pomtestsTW3.LogoutPage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LogoutTestCases {
+
+    private WebDriver driver;
+    private LoginPage loginPage;
+    private DashboardPage dashboardPage;
+    private LogoutPage logoutPage;
+
+    @BeforeEach
+    public void setup() {
+
+        driver = new ChromeDriver();
+        loginPage = new LoginPage(driver);
+        loginPage.login(driver, System.getenv("USER_NAME"), System.getenv("PASSWORD"), "true");
+    }
+
+    @AfterEach
+    void quit() {
+
+        driver.quit();
+    }
+
+    @Test
+    public void logout() {
+
+        dashboardPage.logout(driver);
+        assertTrue(logoutPage.logoutSuccessful());
+    }
 }
