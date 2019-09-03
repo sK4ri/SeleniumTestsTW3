9caf550e98cd3032a57c6e4648e4cdae5448540d
sK4ri <skari.simon@gmail.com>
2019-09-03 09:21:13 +0200

    Logout 1st

---
+++ src/main/java/com/codecanvas/pomtestsTW3/DashboardPage.java
@@ -0,0 +1,11 @@
+package com.codecanvas.pomtestsTW3;
+
+import waiter.Waiter;
+
+public class DashboardPage {
+
+    private Waiter waiter = new Waiter();
+    private final String PAGE_URL = "https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa";
+
+
+}
