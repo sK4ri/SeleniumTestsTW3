85692b9498cac3b3d8f65340267cda3077724ad9
sK4ri <skari.simon@gmail.com>
2019-09-03 09:21:13 +0200

    Logout 1st

---
+++ src/main/java/com/codecanvas/pomtestsTW3/LogoutPage.java
@@ -0,0 +1,9 @@
+package com.codecanvas.pomtestsTW3;
+
+import waiter.Waiter;
+
+public class LogoutPage {
+
+    private Waiter waiter = new Waiter();
+    private final String PAGE_URL = "https://jira.codecool.codecanvas.hu/secure/Logout";
+}
