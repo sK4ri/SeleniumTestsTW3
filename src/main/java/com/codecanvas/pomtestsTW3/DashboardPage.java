2e9a0e9d32f995fa4cc47a62bb4d74db74afe6f6
Lizi Boros <boroslizi@gmail.com>
2019-09-04 11:27:34 +0200

    CreateIssue works + PAGE_URL for dashboard deleted (it's unnecessary as login() already navigates to that URL)

--- src/main/java/com/codecanvas/pomtestsTW3/DashboardPage.java
+++ src/main/java/com/codecanvas/pomtestsTW3/DashboardPage.java
@@ -1,26 +1,32 @@
 package com.codecanvas.pomtestsTW3;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-
 public class DashboardPage {
 
-
-    private final String PAGE_URL = "https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa";
+//    private final String PAGE_URL = "https://jira.codecool.codecanvas.hu/secure/Dashboard.jspa";
 
     @CacheLookup
     @FindBy(id = "header-details-user-fullname") private WebElement userMenu;
     @CacheLookup
     @FindBy(id = "log_out") private WebElement logoutButton;
+    @FindBy(id = "create_link") private WebElement createIssueButton;
+    @FindBy(id = "project-field") private WebElement projectNameInput;
+    @FindBy(id = "summary") private WebElement summaryInput;
+    @FindBy(xpath = "//input[@id='create-issue-submit']") private WebElement createIssueSubmit;
+    @FindBy(xpath = "//*[@id=\"aui-flag-container\"]/div/div/a") private WebElement confirmationLink;
+    @FindBy(id = "summary-val") private WebElement summaryValue;
 
     public DashboardPage(WebDriver driver) {
 
         PageFactory.initElements(driver, this);
-        driver.get(PAGE_URL);
+//        driver.get(PAGE_URL);
     }
 
     public void logout(WebDriver driver) {
@@ -29,5 +35,26 @@ public class DashboardPage {
         userMenu.click();
         Util.waitForWebElementToBeLocated(driver, logoutButton);
         logoutButton.click();
+    }
+
+    public void createIssue(WebDriver driver, String projectName, String summary) {
+        Util.waitForWebElementToBeLocated(driver, createIssueButton);
+        createIssueButton.click();
+        Util.waitForWebElementToBeLocated(driver, projectNameInput);
+        projectNameInput.sendKeys(Keys.chord(Keys.CONTROL, "a"), projectName + Keys.RETURN);
+        Util.waitForWebElementToBeLocated(driver, summaryInput);
+        Actions actions = new Actions(driver);
+        actions.moveToElement(summaryInput);
+        actions.click();
+        actions.sendKeys(summary + Keys.RETURN);
+        actions.build().perform();
+        Util.waitForWebElementToBeLocated(driver, createIssueSubmit);
+        createIssueSubmit.click();
+        Util.waitForWebElementToBeLocated(driver, confirmationLink);
+        confirmationLink.click();
+    }
+
+    public String getSummaryValue() {
+        return summaryValue.getText();
     }
 }
--- src/test/java/CreateIssueTestCases.java
+++ src/test/java/CreateIssueTestCases.java
@@ -27,21 +27,15 @@ public class CreateIssueTestCases {
         driver.quit();
     }
 
+    @Test
+    public void createIssue() {
+        String summary ="123";
+        dashboardPage.createIssue(driver, "Main Testing Project (MTP)", summary);
+        assertEquals(summary, dashboardPage.getSummaryValue());
+    }
 
 
-//    public void checkCreateIssuePossible() {
-//        Wait wait = new FluentWait(driver).ignoring(NoSuchElementException.class).withTimeout(20, TimeUnit.SECONDS);
-//        driver.findElement(By.id("create_link")).click();
-//        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("project-field")));
-//        driver.findElement(By.id("project-field")).sendKeys("Main Testing Project (MTP)" + Keys.RETURN);
-//        wait.until(ExpectedConditions.elementToBeClickable(By.id("summary")));
-//        driver.findElement(By.id("summary")).sendKeys("Create Issue Test");
-//        driver.findElement(By.id("create-issue-submit")).click();
-//        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"aui-flag-container\"]/div/div/a")));
-//        driver.findElement(By.xpath("//*[@id=\"aui-flag-container\"]/div/div/a")).click();
-//        WebElement issueSummary = driver.findElement(By.id("summary-val"));
-//        assertEquals("Create Issue Test", issueSummary.getText());
-//
+
 //        // Delete test issue
 //        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"opsbar-operations_more\"]")));
 //        driver.findElement(By.xpath("//*[@id=\"opsbar-operations_more\"]")).click();
