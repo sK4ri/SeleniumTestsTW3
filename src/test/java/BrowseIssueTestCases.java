58136859c719875101a9a45b0ccda438cc156076
patrickbakos <bakospatrick@gmail.com>
2019-10-02 13:42:08 +0200

    Update BrowseIssueTestCases.java

--- src/test/java/BrowseIssueTestCases.java
+++ src/test/java/BrowseIssueTestCases.java
@@ -14,7 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class BrowseIssueTestCases {
 
-    private WebDriver driver = new ChromeDriver();
+    private WebDriver driver;
     private String nodeURL;
 
     private DashboardPage mainPage = new DashboardPage(driver);
